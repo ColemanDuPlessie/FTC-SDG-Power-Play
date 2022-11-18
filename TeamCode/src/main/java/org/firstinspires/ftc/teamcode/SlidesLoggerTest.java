8be18cd3ac616e55391b492a05ce8835ca1f7ede
ColemanDuPlessie <coleyduplessie@gmail.com>
2022-11-18 14:09:12 -0500

    Updated some quick logging code

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SlidesLoggerTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SlidesLoggerTest.java
@@ -41,47 +41,13 @@ import org.firstinspires.ftc.teamcode.backend.CommandbasedOpmode;
 @TeleOp(name="Slides Logger")
 public class SlidesLoggerTest extends CommandbasedOpmode {
 
-    private double slidesTarget = 0.0;
-
     @Override
     public void init() {
-
         robot.init(hardwareMap, true);
-
     }
-
-    /*
-     * Code to run REPEATEDLY after the driver hits INIT, but before they hit PLAY
-     */
-    @Override
-    public void init_loop() {
-    }
-
-    /*
-     * Code to run ONCE when the driver hits PLAY
-     */
-    @Override
-    public void start() {
-
-    }
-
-    /*
-     * Code to run REPEATEDLY after the driver hits PLAY but before they hit STOP
-     */
+    
     @Override
     public void loop() {
-        if (pad1.getDpadUp()) {slidesTarget = Math.min(slidesTarget+0.0025, 1.0);}
-        if (pad1.getDpadDown()) {slidesTarget = Math.max(slidesTarget-0.0025, 0.0);}
-        robot.slides.setTargetPosition(slidesTarget);
         telemetry.addData("Slides Position", robot.slides.motor.getCurrentPosition());
-        telemetry.addData("Slides Target", slidesTarget);
-
-    }
-
-    /*
-     * Code to run ONCE after the driver hits STOP
-     */
-    @Override
-    public void stop() {
     }
 }
