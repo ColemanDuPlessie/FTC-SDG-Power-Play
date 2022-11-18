6f6e4e819bd4cb5c6f37003ffe91a0d5295ace65
ColemanDuPlessie <coleyduplessie@gmail.com>
2022-11-18 13:56:05 -0500

    Theoretically got the slides working (?)

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/backend/subsystems/SlidesSubsystem.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/backend/subsystems/SlidesSubsystem.java
@@ -1,19 +1,21 @@
 package org.firstinspires.ftc.teamcode.backend.subsystems;
 
 import com.acmerobotics.dashboard.config.Config;
+import com.arcrobotics.ftclib.command.SubsystemBase;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
 import org.firstinspires.ftc.teamcode.AutoToTeleopContainer;
+import org.firstinspires.ftc.teamcode.backend.utilities.PositionControlled;
 import org.firstinspires.ftc.teamcode.backend.utilities.controllers.GravityPIDFController;
 import org.firstinspires.ftc.teamcode.backend.utilities.controllers.PIDController;
 
 @Config
-public class SlidesSubsystem extends Subsystem implements PositionControlled {
+public class SlidesSubsystem extends SubsystemBase implements PositionControlled {
 
-    public DcMotor motor; // TODO
+    public DcMotor motor;
 
     private final PIDController PIDF;
 
@@ -24,14 +26,15 @@ public class SlidesSubsystem extends Subsystem implements PositionControlled {
     public static double kI = 0.0000;
     public static double kD = 0.0001;
     public static double kG = 0.1;
-    public static double powerMultThrottle = 0.75;
+    public static double maxPower = 0.75;
+    public static double edgePower = 0.25;
+    public static int edgeDistance = 100;
 
     private int targetPosition;
 
     private final int startPosition;
 
     public SlidesSubsystem(ElapsedTime aTimer, HardwareMap ahwMap) {
-        super(aTimer, ahwMap);
         motor = ahwMap.get(DcMotor.class, "SlidesMotor");
         motor.setDirection(DcMotorSimple.Direction.FORWARD);
         motor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
@@ -41,7 +44,6 @@ public class SlidesSubsystem extends Subsystem implements PositionControlled {
     }
 
     public SlidesSubsystem(ElapsedTime aTimer, HardwareMap ahwMap, boolean isTeleop) {
-        super(aTimer, ahwMap);
         motor = ahwMap.get(DcMotor.class, "SlidesMotor");
         motor.setDirection(DcMotorSimple.Direction.FORWARD);
         motor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
@@ -60,12 +62,21 @@ public class SlidesSubsystem extends Subsystem implements PositionControlled {
 
     public double getPosition() {return ((double)(motor.getCurrentPosition()-startPosition)-minPosition)/(double)(maxPosition-minPosition);}
 
+    public double getTargetPosition() {return ((double)(targetPosition-startPosition)-minPosition)/(double)(maxPosition-minPosition);}
+
     public void setTargetPosition(double target) {
         targetPosition = (int)(target * (maxPosition-minPosition) + minPosition);
     }
 
+    public void incrementTargetPosition(double increment) {
+        targetPosition += (int)(increment * (maxPosition-minPosition) + minPosition);
+        targetPosition = Math.min(Math.max(targetPosition, minPosition), maxPosition);
+    }
+
     @Override
-    public void update() {
+    public void periodic() {
+        int currentPosition = motor.getCurrentPosition();
+        double powerMultThrottle = edgePower + (maxPower - edgePower) * Math.min(((double)(Math.min(Math.abs(currentPosition-minPosition), Math.abs(currentPosition-maxPosition))))/edgeDistance, 1.0);
         motor.setPower(Math.min(powerMultThrottle, Math.max(PIDF.update(motor.getCurrentPosition()-startPosition, targetPosition) * powerMultThrottle, -powerMultThrottle)));
     }
 
