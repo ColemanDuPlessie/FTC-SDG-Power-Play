acf6a7b8b487bc9fe2470eea1be2e645dae575b2
ColemanDuPlessie <coleyduplessie@gmail.com>
2023-01-19 20:21:21 -0500

    Made slides not go up too far
    Me talk pretty

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/backend/subsystems/SlidesSubsystem.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/backend/subsystems/SlidesSubsystem.java
@@ -21,7 +21,7 @@ public class SlidesSubsystem extends SubsystemBase implements PositionControlled
     private PIDController PIDF;
 
     public static int minPosition = 0; // We don't actually want to go all the way down.
-    public static int maxPosition = 2570;
+    public static int maxPosition = 2370;
 
     public static double kP = 0.006;
     public static double kI = 0.0000;
