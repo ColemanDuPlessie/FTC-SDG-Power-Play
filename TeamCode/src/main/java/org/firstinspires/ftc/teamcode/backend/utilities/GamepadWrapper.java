3b09bc959a0b4018b687eed8a32a42ca620aa0cc
ColemanDuPlessie <coleyduplessie@gmail.com>
2023-02-16 13:55:35 -0500

    Simplified some old gamepad code

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/backend/utilities/GamepadWrapper.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/backend/utilities/GamepadWrapper.java
@@ -30,13 +30,9 @@ public class GamepadWrapper {
     public double getRightTrigger() { return g.right_trigger;}
 
     public boolean getLeftStickButton() { return g.left_stick_button;}
-    public double getLeftStickX() { return g.left_stick_button ? 0 : g.left_stick_x;}
-    public double getLeftStickY() { return g.left_stick_button ? 0 : g.left_stick_y;}
-    public double getAltLeftStickX() { return g.left_stick_button ? g.left_stick_x : 0;}
-    public double getAltLeftStickY() { return g.left_stick_button ? g.left_stick_y : 0;}
+    public double getLeftStickX() { return g.left_stick_x;}
+    public double getLeftStickY() { return g.left_stick_y;}
     public boolean getRightStickButton() { return g.right_stick_button;}
-    public double getRightStickX() { return g.right_stick_button ? 0 : g.right_stick_x;}
-    public double getRightStickY() { return g.right_stick_button ? 0 : g.right_stick_y;}
-    public double getAltRightStickX() { return g.right_stick_button ? g.right_stick_x : 0;}
-    public double getAltRightStickY() { return g.right_stick_button ? g.right_stick_y : 0;}
+    public double getRightStickX() { return g.right_stick_x;}
+    public double getRightStickY() { return g.right_stick_y;}
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/backend/utilities/OverrideGamepadWrapper.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/backend/utilities/OverrideGamepadWrapper.java
@@ -15,12 +15,8 @@ public class OverrideGamepadWrapper extends GamepadWrapper{
 
     public boolean getX() {return g.x && !override.a;}
 
-    public double getLeftStickX() { return !override.a ? ((g.left_stick_button ? 0 : g.left_stick_x) * speedMult) : override.left_stick_x;}
-    public double getLeftStickY() { return !override.a ? ((g.left_stick_button ? 0 : g.left_stick_y) * speedMult) : override.left_stick_y;}
-    public double getAltLeftStickX() { return (g.left_stick_button ? g.left_stick_x : 0) * speedMult;}
-    public double getAltLeftStickY() { return (g.left_stick_button ? g.left_stick_y : 0) * speedMult;}
-    public double getRightStickX() { return !override.a ? ((g.right_stick_button ? 0 : g.right_stick_x) * speedMult) : override.right_stick_x;}
-    public double getRightStickY() { return !override.a ? ((g.right_stick_button ? 0 : g.right_stick_y) * speedMult) : override.right_stick_y;}
-    public double getAltRightStickX() { return (g.right_stick_button ? g.right_stick_x : 0) * speedMult;}
-    public double getAltRightStickY() { return (g.right_stick_button ? g.right_stick_y : 0) * speedMult;}
+    public double getLeftStickX() { return !override.a ? (g.left_stick_x * speedMult) : override.left_stick_x;}
+    public double getLeftStickY() { return !override.a ? (g.left_stick_y * speedMult) : override.left_stick_y;}
+    public double getRightStickX() { return !override.a ? (g.right_stick_x * speedMult) : override.right_stick_x;}
+    public double getRightStickY() { return !override.a ? (g.right_stick_y * speedMult) : override.right_stick_y;}
 }
